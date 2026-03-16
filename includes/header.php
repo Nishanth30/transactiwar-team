71988135d0067374423d895b72346e64afa7b87b
TejasDapse <134951412+TejasDapse@users.noreply.github.com>
2026-03-16 18:24:01 +0530

    Add SRI hashes to vendor scripts in header.php
    Add Subresource Integrity (SRI) hashes to vendor CSS
    
    - Added `integrity` (SHA-384 hash) and `crossorigin="anonymous"` attributes to the Bootstrap CSS link in [includes/header.php]
    - Mitigates supply chain attacks by preventing the browser from executing modified or malicious scripts injected by a compromised CDN.

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1)
+;
 
 require_once __DIR__ . '/request.php';
 
@@ -216,6 +217,6 @@ function render_page_head(string $title, string $extraHead = ''): void
     if ($extraHead !== '') {
         echo $extraHead . PHP_EOL;
     }
-    echo '<link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">' . PHP_EOL;
+    echo '<link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css" integrity="sha384-iyVHgWdgbOYA1Qxlw7jRhcp52l0uPEs4zrd0MRrcr1fDCFRPyz0k1VrxB1urC55B" crossorigin="anonymous">' . PHP_EOL;
     echo '<link rel="stylesheet" href="/assets/css/style.css">' . PHP_EOL;
 }
